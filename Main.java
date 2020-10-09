a595e7947412bd32b3e922c520609e2d119b358b
Sardaribrahimajab <isardar784@gmail.com>
2020-10-09 11:14:44 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SingleObject object = SingleObject.getInstance();
+        object.showMessage();
+    }
+}
